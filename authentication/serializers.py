a3a5313270825ef3a761b4baf1b5504ccad7a1d4
aadityas201 <69757025+aadityas201@users.noreply.github.com>
2022-04-04 17:07:46 +0530

    Login Done

---
+++ authentication/serializers.py
@@ -0,0 +1,63 @@
+from rest_framework import serializers
+from .models import User
+from rest_framework.exceptions import AuthenticationFailed
+from django.contrib import auth
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length=68, min_length=6, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password','mobile_no', 'name']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class EmailVerificationSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(max_length=555)
+
+    class Meta:
+        model = User
+        fields = ['token']
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=3)
+    password = serializers.CharField(
+        max_length=68, min_length=6, write_only=True)
+    tokens = serializers.SerializerMethodField()
+
+    def get_tokens(self, obj):
+        user = User.objects.get(email=obj['email'])
+
+        return {
+            'refresh': user.tokens()['refresh'],
+            'access': user.tokens()['access']
+        }
+
+    class Meta:
+        model = User
+        fields = ['email', 'password','tokens']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+        filtered_user_by_email = User.objects.filter(email=email)
+        user = auth.authenticate(email=email, password=password)
+
+        if not user:
+            raise AuthenticationFailed('Invalid credentials')
+        if not user.is_active:
+            raise AuthenticationFailed('Your account is disabled')
+        if not user.is_verified:
+            raise AuthenticationFailed('Email is not verified')
+
+        return {
+            'email': user.email,
+            'name': user.name,
+            'tokens': user.tokens
+        }
+
+        return super().validate(attrs)
